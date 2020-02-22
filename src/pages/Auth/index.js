a285373a826881bef677ee573195f5c3800180cf
Илья Долженко <ilay00@mail.ru>
2020-02-22 23:26:47 +0300

    init

---
+++ src/pages/Auth/index.js
@@ -0,0 +1,30 @@
+import React from 'react'
+import { Form, Input, Icon } from 'antd';
+
+import { ShadowBlock, Button } from 'components';
+import './Auth.scss';
+
+const Auth = () => (
+    <section className='auth'>
+        <div className='auth-content'>
+            <div className='auth-content__header'>
+                <h2>Войти в чат</h2>
+                <p>Пожалуйста, введите никнейм</p>
+            </div>
+            <ShadowBlock>
+                <Form className="login-form">
+                    <Form.Item>
+                        <Input
+                            prefix={<Icon type="user" style={{ color: 'rgba(0,0,0,.25)' }} />}
+                            size='large'
+                            placeholder="Никнейм"
+                        />
+                    </Form.Item>
+                    <Button type='primary' size='large'>Войти в чат</Button>
+                </Form>
+            </ShadowBlock>
+        </div>
+    </section>
+);
+
+export default Auth
